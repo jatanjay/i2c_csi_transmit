e93865dae45999e71c4385d9daab3bdcd76eca1e
Jatan Pandya <jatanjay212@gmail.com>
2024-04-01 16:21:23 -0400

    4/1/2024

--- csi_transmit.py
+++ csi_transmit.py
@@ -2,10 +2,9 @@ import smbus
 import time
 
 bus_number = 1
-device_address = 0x48
 bus = smbus.SMBus(bus_number)
 
-def write_command(register, data):
+def write_command(device_address, register, data):
     bus.write_byte_data(device_address, register, data) 
     time.sleep(0.01)  
 
@@ -19,49 +18,153 @@ def send_commands(mode_commands):
                 device_address = ''.join(command.strip().split(';')[0].strip().split()[0:1])
                 register = byte[0]
                 data = byte[1]
-
                 print(device_address, register, data)
-                write_command(device_address, register, data)
+                # write_command(device_address, register, data)
 
 
-modes = [
-    "Color Bars 576i MIPI Out:\ndelay 10 ;  \n42 0F 00 ; Exit Power Down Mode\n42 00 04 ; ADI Required Write \n42 0C 37 ; Force Free-run mode \n42 02 84 ; Force standard to PAL \n42 14 11 ; Set Free-run pattern to color bars \n42 03 4E ; ADI Required Write \n42 04 57 ; Power-up INTRQ pin\n42 13 00 ; Enable INTRQ output driver\n42 17 41 ; select SH1\n42 1D C0 ; Tri-State LLC output driver\n42 52 CB ; ADI Required Write\n42 80 51 ; ADI Required Write\n42 81 51 ; ADI Required Write\n42 82 68 ; ADI Required Write\n42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V\n42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V\n42 FE 88 ; Set CSI Map Address\n88 DE 02 ; Power up MIPI D-PHY\n88 D2 F7 ; ADI Required Write\n88 D8 65 ; ADI Required Write\n88 E0 09 ; ADI Required Write \n88 2C 00 ; ADI Required Write\n88 00 00 ; Power up MIPI CSI-2 Tx --done--\nEnd",
-    "Color Bars 480i MIPI Out:\ndelay 10 ; \n42 0F 00 ; Exit Power Down Mode\n42 00 04 ; ADI Required Write \n42 0C 37 ; Force Free-run mode \n42 02 54 ; Force standard to NTSC-M \n42 14 11 ; Set Free-run pattern to color bars \n42 03 4E ; ADI Required Write \n42 04 57 ; Power-up INTRQ pin\n42 13 00 ; Enable INTRQ output driver\n42 17 41 ; select SH1\n42 1D C0 ; Tri-State LLC output driver\n42 52 CB ; ADI Required Write\n42 80 51 ; ADI Required Write\n42 81 51 ; ADI Required Write\n42 82 68 ; ADI Required Write\n42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V\n42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V\n42 FE 88 ; Set CSI Map Address\n88 DE 02 ; Power up MIPI D-PHY\n88 D2 F7 ; ADI Required Write\n88 D8 65 ; ADI Required Write\n88 E0 09 ; ADI Required Write \n88 2C 00 ; ADI Required Write\n88 00 00 ; Power up MIPI CSI-2 Tx --done--\nEnd",
-    "Color Bars 576p MIPI Out:\ndelay 10 ;\n42 0F 00 ; Exit power down mode\n42 00 04 ; ADI Required Write \n42 0C 37 ; Force Free-run mode \n42 02 84 ; Force standard to PAL \n42 14 11 ; Set Free-run pattern to color bars\n42 03 4E ; ADI Required Write\n42 04 57 ; Enable Intrq pin\n42 13 00 ; Enable INTRQ output driver\n42 17 41 ; select SH1\n42 1D C0 ; Tri-State LLC output driver\n42 52 CB ; ADI Required Write\n42 80 51 ; ADI Required Write\n42 81 51 ; ADI Required Write\n42 82 68 ; ADI Required Write\n42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V\n42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V\n42 FD 84 ; Set VPP Map Address\n84 A3 00 ; ADI Required Write\n84 5B 00 ; Advanced Timing Enabled\n84 55 80 ; Enable I2P\n42 FE 88 ; Set CSI Map Address\n88 01 20 ; ADI Required Write\n88 02 28 ; ADI Required Write\n88 03 38 ; ADI Required Write\n88 04 30 ; ADI Required Write\n88 05 30 ; ADI Required Write\n88 06 80 ; ADI Required Write\n88 07 70 ; ADI Required Write\n88 08 50 ; ADI Required Write\n88 DE 02 ; Power up D-PHY\n88 D2 F7 ; ADI Required Write\n88 D8 65 ; ADI Required Write\n88 E0 09 ; ADI Required Write\n88 2C 00 ; ADI Required Write\n88 1D 80 ; ADI Required Write\n88 00 00 ; Power up MIPI CSI-2 Tx --done--\nEnd"
-]
+modes = {"Color Bars 576i MIPI Out":""":Color Bars 576i MIPI Out:
+delay 10 ; 	
+42 0F 00 ; Exit Power Down Mode
+42 00 04 ; ADI Required Write 
+42 0C 37 ; Force Free-run mode 
+42 02 84 ; Force standard to PAL 
+42 14 11 ; Set Free-run pattern to color bars 
+42 03 4E ; ADI Required Write 
+42 04 57 ; Power-up INTRQ pin
+42 13 00 ; Enable INTRQ output driver
+42 17 41 ; select SH1
+42 1D C0 ; Tri-State LLC output driver
+42 52 CB ; ADI Required Write
+42 80 51 ; ADI Required Write
+42 81 51 ; ADI Required Write
+42 82 68 ; ADI Required Write
+42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V
+42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V
+42 FE 88 ; Set CSI Map Address
+88 DE 02 ; Power up MIPI D-PHY
+88 D2 F7 ; ADI Required Write
+88 D8 65 ; ADI Required Write
+88 E0 09 ; ADI Required Write 
+88 2C 00 ; ADI Required Write
+88 00 00 ; Power up MIPI CSI-2 Tx --done--
+End""",
+"CVBS Single Ended In Ain 1":""":AUTODETECT CVBS Single Ended In Ain 1, MIPI Out:
+delay 10 ; 
+42 0F 00 ; Exit Power Down Mode
+42 00 00 ; INSEL = CVBS in on Ain 1
+42 0E 80 ; ADI Required Write 
+42 9C 00 ; ADI Required Write 
+42 9C FF ; ADI Required Write 
+42 0E 00 ; Enter User Sub Map
+42 03 4E ; ADI Required Write 
+42 04 57 ; Power-up INTRQ pin
+42 13 00 ; Enable INTRQ output driver
+42 17 41 ; select SH1
+42 1D C0 ; Tri-State LLC output driver
+42 52 CB ; ADI Required Write
+42 80 51 ; ADI Required Write
+42 81 51 ; ADI Required Write
+42 82 68 ; ADI Required Write
+42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V
+42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V
+42 FE 88 ; Set CSI Map Address
+88 DE 02 ; Power up MIPI D-PHY
+88 D2 F7 ; ADI Required Write
+88 D8 65 ; ADI Required Write
+88 E0 09 ; ADI Required Write 
+88 2C 00 ; ADI Required Write
+88 00 00 ; Power up MIPI CSI-2 Tx --done--
+End""",
+"CVBS Single Ended In Ain 2":"""AUTODETECT CVBS Single Ended In Ain 2, MIPI Out: 
+delay 10 ; 
+42 0F 00 ; Exit Power Down Mode
+42 00 01 ; INSEL = CVBS in on Ain 2
+42 0E 80 ; ADI Required Write 
+42 9C 00 ; ADI Required Write 
+42 9C FF ; ADI Required Write 
+42 0E 00 ; Enter User Sub Map
+42 03 4E ; ADI Required Write 
+42 04 57 ; Enable Intrq pin
+42 13 00 ; Enable INTRQ output driver
+42 17 41 ; select SH1
+42 1D C0 ; Tri-State LLC output driver
+42 52 CB ; ADI Required Write
+42 80 51 ; ADI Required Write
+42 81 51 ; ADI Required Write
+42 82 68 ; ADI Required Write
+42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V
+42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V
+42 FE 88 ; Set CSI Map Address
+88 DE 02 ; Power up MIPI D-PHY
+88 D2 F7 ; ADI Required Write
+88 D8 65 ; ADI Required Write
+88 E0 09 ; ADI Required Write 
+88 2C 00 ; ADI Required Write
+88 00 00 ; Power up MIPI CSI-2 Tx --done--
+End""",
+"CVBS Single Ended In Ain 3":"""AUTODETECT CVBS Single Ended In Ain 3, MIPI Out: 
+delay 10 ; 
+42 0F 00 ; Exit Power Down Mode
+42 00 02 ; INSEL = CVBS in on Ain 3
+42 0E 80 ; ADI Required Write 
+42 9C 00 ; ADI Required Write 
+42 9C FF ; ADI Required Write 
+42 0E 00 ; Enter User Sub Map
+42 03 4E ; ADI Required Write 
+42 04 57 ; Enable Intrq pin
+42 13 00 ; Enable INTRQ output driver
+42 17 41 ; select SH1
+42 1D C0 ; Tri-State LLC output driver
+42 52 CB ; ADI Required Write
+42 80 51 ; ADI Required Write
+42 81 51 ; ADI Required Write
+42 82 68 ; ADI Required Write
+42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V
+42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V
+42 FE 88 ; Set CSI Map Address
+88 DE 02 ; Power up MIPI D-PHY
+88 D2 F7 ; ADI Required Write
+88 D8 65 ; ADI Required Write
+88 E0 09 ; ADI Required Write 
+88 2C 00 ; ADI Required Write
+88 00 00 ; Power up MIPI CSI-2 Tx --done--
+End""",
+"CVBS Single Ended In Ain 4":"""AUTODETECT CVBS Single Ended In Ain 4, MIPI Out: 
+delay 10 ; 
+42 0F 00 ; Exit Power Down Mode
+42 00 03 ; INSEL = CVBS in on Ain 4
+42 0E 80 ; ADI Required Write 
+42 9C 00 ; ADI Required Write 
+42 9C FF ; ADI Required Write 
+42 0E 00 ; Enter User Sub Map
+42 03 4E ; ADI Required Write 
+42 04 57 ; Enable Intrq pin
+42 13 00 ; Enable INTRQ output driver
+42 17 41 ; select SH1
+42 1D C0 ; Tri-State LLC output driver
+42 52 CB ; ADI Required Write
+42 80 51 ; ADI Required Write
+42 81 51 ; ADI Required Write
+42 82 68 ; ADI Required Write
+42 5D 3C ; Enable Diagnostic pin 1 - Level=1.125V
+42 5E 3C ; Enable Diagnostic pin 2 - Level=1.125V
+42 FE 88 ; Set CSI Map Address
+88 DE 02 ; Power up MIPI D-PHY
+88 D2 F7 ; ADI Required Write
+88 D8 65 ; ADI Required Write
+88 E0 09 ; ADI Required Write 
+88 2C 00 ; ADI Required Write
+88 00 00 ; Power up MIPI CSI-2 Tx --done--
+End""",
+}
 
-for mode in modes:
-    send_commands(mode)
 
 
-# import time
-# import smbus2
+print("Choose a mode:")
+for idx, mode in enumerate(modes):
+    print(f"{idx + 1}: {mode}")
 
-# DEVICE_ADDRESS = 0x48
-# COMMAND_DELAY = 0
+selected_mode_index = int(input("Enter the mode number: ")) - 1
+selected_mode = list(modes.keys())[selected_mode_index]
 
-# with open("/home/qt/Desktop/i2c_csi_transmit/ADV7282M_Cust-VER.4.5[1].txt", "r") as file:
-#     commands = file.readlines()
-
-# bus = smbus2.SMBus(1)
-
-# for command in commands:
-#     if not (command.startswith(";") or command.startswith("#") or command.startswith(":")):
-#         pass
-#     else:
-#         print(command)
-
-
-
-
-
-
-#     # command_bytes = [int(byte, 16) for byte in command.strip().split()]
-    
-#     # for byte in command_bytes:
-#     #     bus.write_byte(DEVICE_ADDRESS, byte)
-#     #     print(DEVICE_ADDRESS, byte)
-#     #     time.sleep(COMMAND_DELAY)
-
-# bus.close()
-
+send_commands(modes[selected_mode])
